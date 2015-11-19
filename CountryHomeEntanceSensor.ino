53b901be9864b66b9e1cdef0fd7fcdbab378540d
Dmitry Moshchalkov <damsh@me.com>
2015-11-19 20:51:24 +0300

    Improved motion sensor state reporting

--- CountryHomeEntanceSensor.ino
+++ CountryHomeEntanceSensor.ino
@@ -240,6 +240,8 @@ void incomingMessage(const MyMessage &message) {
             boolMotionSensorDisabled = false;
          }
 
+         gw.send(MotionStateMsg.set(boolMotionSensorDisabled ? "1" : "0" )); 
+
      }
 
     if ( message.sensor == RECHECK_SENSOR_VALUES ) {
